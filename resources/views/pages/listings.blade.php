42a183c8abf420e2d751a96ac0229e4b0bd6a745
Desi Steib <desiwebdev11@gmail.com>
2022-03-02 02:02:17 -0500

    Created listings properties images and made responsive (bootstrap).

--- resources/views/pages/listings.blade.php
+++ resources/views/pages/listings.blade.php
@@ -32,6 +32,304 @@
       </a>
      </div>
     </div>
+    <div class="listings-properties">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6 col-lg-4 col-xl-3">
+            <div class="listings-properties__item">
+              <img src="https://images.pexels.com/photos/7031595/pexels-photo-7031595.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="item image">
+              <span class="listings-properties__item-price">
+                $300,000
+              </span>
+              <span class="listings-properties__item-details">
+                <i class="fa-solid fa-bed"></i>
+                4
+                <i class="fa-solid fa-shower"></i>
+                3
+                <i class="fa-solid fa-ruler-combined"></i>
+                2550 SQFT
+              </span>
+              <span class="listings-properties__item-address">
+                221 piedmont rd <br>
+                buckhead, ga 11222
+              </span>
+              <div class="listings-properties__item-line"></div>
+              <span class="listings-properties__item-agent">
+                stephon rhea
+              </span>
+            </div>
+          </div>
+          <div class="col-sm-6 col-lg-4 col-xl-3">
+            <div class="listings-properties__item">
+              <img src="https://images.pexels.com/photos/7031595/pexels-photo-7031595.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="item image">
+              <span class="listings-properties__item-price">
+                $300,000
+              </span>
+              <span class="listings-properties__item-details">
+                <i class="fa-solid fa-bed"></i>
+                4
+                <i class="fa-solid fa-shower"></i>
+                3
+                <i class="fa-solid fa-ruler-combined"></i>
+                2550 SQFT
+              </span>
+              <span class="listings-properties__item-address">
+                221 piedmont rd <br>
+                buckhead, ga 11222
+              </span>
+              <div class="listings-properties__item-line"></div>
+              <span class="listings-properties__item-agent">
+                stephon rhea
+              </span>
+            </div>
+          </div>
+          <div class="col-sm-6 col-lg-4 col-xl-3">
+            <div class="listings-properties__item">
+              <img src="https://images.pexels.com/photos/7031595/pexels-photo-7031595.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="item image">
+              <span class="listings-properties__item-price">
+                $300,000
+              </span>
+              <span class="listings-properties__item-details">
+                <i class="fa-solid fa-bed"></i>
+                4
+                <i class="fa-solid fa-shower"></i>
+                3
+                <i class="fa-solid fa-ruler-combined"></i>
+                2550 SQFT
+              </span>
+              <span class="listings-properties__item-address">
+                221 piedmont rd <br>
+                buckhead, ga 11222
+              </span>
+              <div class="listings-properties__item-line"></div>
+              <span class="listings-properties__item-agent">
+                stephon rhea
+              </span>
+            </div>
+          </div>
+          <div class="col-sm-6 col-lg-4 col-xl-3">
+            <div class="listings-properties__item">
+              <img src="https://images.pexels.com/photos/7031595/pexels-photo-7031595.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="item image">
+              <span class="listings-properties__item-price">
+                $300,000
+              </span>
+              <span class="listings-properties__item-details">
+                <i class="fa-solid fa-bed"></i>
+                4
+                <i class="fa-solid fa-shower"></i>
+                3
+                <i class="fa-solid fa-ruler-combined"></i>
+                2550 SQFT
+              </span>
+              <span class="listings-properties__item-address">
+                221 piedmont rd <br>
+                buckhead, ga 11222
+              </span>
+              <div class="listings-properties__item-line"></div>
+              <span class="listings-properties__item-agent">
+                stephon rhea
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-6 col-lg-4 col-xl-3">
+            <div class="listings-properties__item">
+              <img src="https://images.pexels.com/photos/7031595/pexels-photo-7031595.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="item image">
+              <span class="listings-properties__item-price">
+                $300,000
+              </span>
+              <span class="listings-properties__item-details">
+                <i class="fa-solid fa-bed"></i>
+                4
+                <i class="fa-solid fa-shower"></i>
+                3
+                <i class="fa-solid fa-ruler-combined"></i>
+                2550 SQFT
+              </span>
+              <span class="listings-properties__item-address">
+                221 piedmont rd <br>
+                buckhead, ga 11222
+              </span>
+              <div class="listings-properties__item-line"></div>
+              <span class="listings-properties__item-agent">
+                stephon rhea
+              </span>
+            </div>
+          </div>
+          <div class="col-sm-6 col-lg-4 col-xl-3">
+            <div class="listings-properties__item">
+              <img src="https://images.pexels.com/photos/7031595/pexels-photo-7031595.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="item image">
+              <span class="listings-properties__item-price">
+                $300,000
+              </span>
+              <span class="listings-properties__item-details">
+                <i class="fa-solid fa-bed"></i>
+                4
+                <i class="fa-solid fa-shower"></i>
+                3
+                <i class="fa-solid fa-ruler-combined"></i>
+                2550 SQFT
+              </span>
+              <span class="listings-properties__item-address">
+                221 piedmont rd <br>
+                buckhead, ga 11222
+              </span>
+              <div class="listings-properties__item-line"></div>
+              <span class="listings-properties__item-agent">
+                stephon rhea
+              </span>
+            </div>
+          </div>
+          <div class="col-sm-6 col-lg-4 col-xl-3">
+            <div class="listings-properties__item">
+              <img src="https://images.pexels.com/photos/7031595/pexels-photo-7031595.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="item image">
+              <span class="listings-properties__item-price">
+                $300,000
+              </span>
+              <span class="listings-properties__item-details">
+                <i class="fa-solid fa-bed"></i>
+                4
+                <i class="fa-solid fa-shower"></i>
+                3
+                <i class="fa-solid fa-ruler-combined"></i>
+                2550 SQFT
+              </span>
+              <span class="listings-properties__item-address">
+                221 piedmont rd <br>
+                buckhead, ga 11222
+              </span>
+              <div class="listings-properties__item-line"></div>
+              <span class="listings-properties__item-agent">
+                stephon rhea
+              </span>
+            </div>
+          </div>
+          <div class="col-sm-6 col-lg-4 col-xl-3">
+            <div class="listings-properties__item">
+              <img src="https://images.pexels.com/photos/7031595/pexels-photo-7031595.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="item image">
+              <span class="listings-properties__item-price">
+                $300,000
+              </span>
+              <span class="listings-properties__item-details">
+                <i class="fa-solid fa-bed"></i>
+                4
+                <i class="fa-solid fa-shower"></i>
+                3
+                <i class="fa-solid fa-ruler-combined"></i>
+                2550 SQFT
+              </span>
+              <span class="listings-properties__item-address">
+                221 piedmont rd <br>
+                buckhead, ga 11222
+              </span>
+              <div class="listings-properties__item-line"></div>
+              <span class="listings-properties__item-agent">
+                stephon rhea
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-6 col-lg-4 col-xl-3">
+            <div class="listings-properties__item">
+              <img src="https://images.pexels.com/photos/7031595/pexels-photo-7031595.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="item image">
+              <span class="listings-properties__item-price">
+                $300,000
+              </span>
+              <span class="listings-properties__item-details">
+                <i class="fa-solid fa-bed"></i>
+                4
+                <i class="fa-solid fa-shower"></i>
+                3
+                <i class="fa-solid fa-ruler-combined"></i>
+                2550 SQFT
+              </span>
+              <span class="listings-properties__item-address">
+                221 piedmont rd <br>
+                buckhead, ga 11222
+              </span>
+              <div class="listings-properties__item-line"></div>
+              <span class="listings-properties__item-agent">
+                stephon rhea
+              </span>
+            </div>
+          </div>
+          <div class="col-sm-6 col-lg-4 col-xl-3">
+            <div class="listings-properties__item">
+              <img src="https://images.pexels.com/photos/7031595/pexels-photo-7031595.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="item image">
+              <span class="listings-properties__item-price">
+                $300,000
+              </span>
+              <span class="listings-properties__item-details">
+                <i class="fa-solid fa-bed"></i>
+                4
+                <i class="fa-solid fa-shower"></i>
+                3
+                <i class="fa-solid fa-ruler-combined"></i>
+                2550 SQFT
+              </span>
+              <span class="listings-properties__item-address">
+                221 piedmont rd <br>
+                buckhead, ga 11222
+              </span>
+              <div class="listings-properties__item-line"></div>
+              <span class="listings-properties__item-agent">
+                stephon rhea
+              </span>
+            </div>
+          </div>
+          <div class="col-sm-6 col-lg-4 col-xl-3">
+            <div class="listings-properties__item">
+              <img src="https://images.pexels.com/photos/7031595/pexels-photo-7031595.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="item image">
+              <span class="listings-properties__item-price">
+                $300,000
+              </span>
+              <span class="listings-properties__item-details">
+                <i class="fa-solid fa-bed"></i>
+                4
+                <i class="fa-solid fa-shower"></i>
+                3
+                <i class="fa-solid fa-ruler-combined"></i>
+                2550 SQFT
+              </span>
+              <span class="listings-properties__item-address">
+                221 piedmont rd <br>
+                buckhead, ga 11222
+              </span>
+              <div class="listings-properties__item-line"></div>
+              <span class="listings-properties__item-agent">
+                stephon rhea
+              </span>
+            </div>
+          </div>
+          <div class="col-sm-6 col-lg-4 col-xl-3">
+            <div class="listings-properties__item">
+              <img src="https://images.pexels.com/photos/7031595/pexels-photo-7031595.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="item image">
+              <span class="listings-properties__item-price">
+                $300,000
+              </span>
+              <span class="listings-properties__item-details">
+                <i class="fa-solid fa-bed"></i>
+                4
+                <i class="fa-solid fa-shower"></i>
+                3
+                <i class="fa-solid fa-ruler-combined"></i>
+                2550 SQFT
+              </span>
+              <span class="listings-properties__item-address">
+                221 piedmont rd <br>
+                buckhead, ga 11222
+              </span>
+              <div class="listings-properties__item-line"></div>
+              <span class="listings-properties__item-agent">
+                stephon rhea
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 @endsection
 
